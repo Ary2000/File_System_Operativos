f12b0c5320e879f9135a897ce61c23398c981bef
Irsacfac <irsacfac@gmail.com>
2022-06-14 22:26:41 -0600

    Update Arbol.java

--- Fyle_system/src/Tree/Arbol.java
+++ Fyle_system/src/Tree/Arbol.java
@@ -16,5 +16,26 @@ public class Arbol {
     
     public Arbol(){
         root = new Directory("File System");
+        this.actualDirectory = root;
+    }
+    
+    public boolean findDirectory(String dirName){
+        return this.actualDirectory.directoryExist(dirName);
+    }
+    
+    public boolean findFile(String fileName){
+        return this.actualDirectory.fileExist(fileName);
+    }
+    
+    public boolean createDirectory(String dirName, Directory actualDirectory){
+        return false;
+    }
+    
+    public boolean createFile(String fileName, String extension, String content){
+        return false;
+    }
+    
+    public void goToRoot(){
+        this.actualDirectory = root;
     }
 }
